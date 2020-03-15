33ac988b953c99e336dd2a244abdc52919e40e68
Anastasia Oshurkova <adoshurkova@gmail.com>
2020-03-15 05:16:49 +0300

    init: started Singolo DOM task

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 'use strict';
 
+const MENU = document.getElementById('menu');
+const TABS = document.getElementById('tabs_menu');
+const PROJECTS = document.getElementById('projects');
+
 //Реализуем активные пункты меню при перемещении по ним
-function chooseNavElement(el) {
-    el = document.querySelectorAll('.navigation__link');
 
+MENU.addEventListener('click', (event) => {
+    MENU.querySelectorAll('a').forEach(el => el.classList.remove('navigation__link_state_active'));
 
-}
+    event.target.classList.add('navigation__link_state_active');
+});
+
 
 //Переключение слайдов бесконечной каруселькой
 
 //Активация телефонов слайда1 (экран гаснет)
 
-//Переключение табов Portfolio
 
-//Рандомное перемещение картинок Portfolio
+//Переключение табов Portfolio и рандомное перемещение картинок Portfolio
+function chooseTabAndMakeRandom(event) {
+    if (event.target.tagName === 'BUTTON') {
+        TABS.querySelectorAll('button').forEach(el =>
+            el.classList.remove('portfolio__tabs-item_active'));
+        event.target.classList.add('portfolio__tabs-item_active');
+        event.stopPropagation();
+
+        let arrOfSrc = [];
+        PROJECTS.querySelectorAll('img').forEach((el) => {
+            arrOfSrc.push(el.src);
+            el.src = '';
+        })
+        let randomArr = arrOfSrc.sort(() =>  Math.random() - 0.5);
+        PROJECTS.querySelectorAll('img').forEach((el, i) => el.src = randomArr[i]);
+    }
+}
+
+TABS.addEventListener('click', chooseTabAndMakeRandom);
+
+//Добавляем рамку выбранному проекту Portfolio
+PROJECTS.addEventListener('click', event => {
+    if (event.target.tagName === 'IMG') {
+        PROJECTS.querySelectorAll('img').forEach(el => {
+            el.classList.remove('portfolio__photo-grid-item_active');
+        });
+        event.target.classList.add('portfolio__photo-grid-item_active');
+    }
+});
 
 //Отменяем отправление данных по кнопке Сабмит. Добавляем окно с отправленными опциями поверх.
 
